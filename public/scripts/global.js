e75b7bfa21e131213ac53159828fcf622f019553
Daniel Aoulou <aouloudan@icloud.com>
2023-07-07 18:20:25 -0400

    added responsive resizing for smaller screen sizes

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -4,3 +4,15 @@ document.querySelectorAll(".text-logo").forEach((item) => {
     window.location.href = "https://fullstackdan.dev";
   });
 });
+
+document
+  .querySelector(".hamburger-menu")
+  .addEventListener("click", function () {
+    var navLinks = document.querySelector(".nav-links");
+    if (navLinks.style.display === "none") {
+      navLinks.style.display = "flex";
+      navLinks.style.flexDirection = "column";
+    } else {
+      navLinks.style.display = "none";
+    }
+  });
